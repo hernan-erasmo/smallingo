146e7f925fbc16abbf9d678ea099ac940087c8bf
Hernán Erasmo <dherasmo@gmail.com>
2024-03-08 12:06:27 -0300

    refactor: update variable names

--- core/tasks.py
+++ core/tasks.py
@@ -12,10 +12,10 @@ from utils import sanitize_filename
 
 # TODO(Hernan) need to find a way to normalize original video names to be consistent
 # with their parsed content, and also to avoid having duplicate videos.
-def download_original_video(model_id: int) -> Optional[str]:
+def download_original_video(video_id: int) -> Optional[str]:
     try:
-        model_instance = SmallingoVideo.objects.get(id=model_id)
-        file_url = model_instance.url
+        smallingo_video = SmallingoVideo.objects.get(id=video_id)
+        file_url = smallingo_video.url
 
         print(f"GETting file at {file_url}")
         response = requests.get(file_url)
@@ -23,10 +23,10 @@ def download_original_video(model_id: int) -> Optional[str]:
             print(f"Successfully got {file_url}")
             file_content = response.content
             file_name = f"{sanitize_filename(os.path.basename(file_url))}_{datetime.now().strftime('%y%m%d%H%M%S%f')}"
-            model_instance.uploaded_file.save(file_name, ContentFile(file_content), save=True)
-            model_instance.save()
+            smallingo_video.uploaded_file.save(file_name, ContentFile(file_content), save=True)
+            smallingo_video.save()
             return file_name
         else:
             print(f"Failed to download file from {file_url}: HTTP status code {response.status_code}")
     except SmallingoVideo.DoesNotExist:
-        print(f"Model with id={model_id} does not exist.")
+        print(f"Model with id={video_id} does not exist.")
