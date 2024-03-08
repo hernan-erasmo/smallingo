76a9bb7ca7b9e21499ca0fe821f0a1511fa9a62d
Hernán Erasmo <dherasmo@gmail.com>
2024-03-08 07:09:06 -0300

    feat: use dotenv to load .env

--- smallingo/settings.py
+++ smallingo/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+env_path = Path('.', '.env')
+load_dotenv(dotenv_path=env_path)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
