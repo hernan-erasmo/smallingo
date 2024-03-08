005160c6eb8f7cb94cc490c65619c2450b0becf4
Hernán Erasmo <dherasmo@gmail.com>
2024-03-08 07:07:28 -0300

    feat: add missing attributes for file fields

--- core/models.py
+++ core/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+AUDIO_DIR = "original_audio"
+AUDIO_TRANSLATION_DIR = "translation_audio"
+THUMBNAIL_DIR = "thumbnails"
+
 class SmallingoVideo(models.Model):
     # checkpoint 1
     url = models.URLField(max_length=65535)
@@ -9,16 +13,16 @@ class SmallingoVideo(models.Model):
     pixels_tall = models.PositiveIntegerField(blank=True, null=True)
     
     # checkpoint 3
-    audio_fragment = models.FileField(blank=True, null=True)
+    audio_fragment = models.FileField(blank=True, null=True, upload_to=AUDIO_DIR)
     
     # checkpoint 4
     audio_fragment_text = models.TextField(blank=True, null=True)
     
     # checkpoint 5
-    audio_fragment_speech = models.FileField(blank=True, null=True)
+    audio_fragment_speech = models.FileField(blank=True, null=True, upload_to=AUDIO_TRANSLATION_DIR)
     
     # checkpoint 6
-    video_thumbnail = models.ImageField(blank=True, null=True)
+    video_thumbnail = models.ImageField(blank=True, null=True, upload_to=THUMBNAIL_DIR)
     
     # checkpoint 7
     video_ocr = models.TextField(blank=True, null=True)
