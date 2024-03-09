4b2b076667eb8e3469d42e40d50cabba3f6e3229
Hernán Erasmo <dherasmo@gmail.com>
2024-03-09 19:39:50 -0300

    feat: convert download_video to celery task with progress

--- backend/core/tasks.py
+++ backend/core/tasks.py
@@ -4,27 +4,35 @@ from datetime import datetime, timedelta
 from typing import Optional
 
 import requests
+from celery import shared_task
 from moviepy.editor import VideoFileClip, AudioFileClip
 
 from django.core.files.base import ContentFile
+from core import app as celery_app
 from core.models import SmallingoVideo
 from core.utils import sanitize_filename
 
+
 # TODO(Hernan) need to find a way to normalize original video names to be consistent
 # with their parsed content, and also to avoid having duplicate videos.
-def download_original_video(video_id: int) -> Optional[str]:
+@celery_app.task(bind=True)
+def download_original_video(self, video_id: int) -> Optional[str]:
     try:
+        self.update_state(state='PROGRESS', meta={'progress': 0})
         smallingo_video = SmallingoVideo.objects.get(id=video_id)
         file_url = smallingo_video.url
 
         print(f"GETting file at {file_url}")
+        self.update_state(state='PROGRESS', meta={'progress': 25})
         with requests.get(file_url, stream=True) as response:
             if response.status_code == 200:
                 print(f"Successfully got {file_url}")
+                self.update_state(state='PROGRESS', meta={'progress': 50})
                 file_content = response.content
                 file_name = f"{sanitize_filename(os.path.basename(file_url))}_{datetime.now().strftime('%y%m%d%H%M%S%f')}"
                 smallingo_video.uploaded_file.save(file_name, ContentFile(file_content), save=True)
                 smallingo_video.save()
+                self.update_state(state='PROGRESS', meta={'progress': 75})
                 return file_name
             else:
                 print(f"Failed to download file from {file_url}: HTTP status code {response.status_code}")
--- backend/core/utils.py
+++ backend/core/utils.py
@@ -1,3 +1,20 @@
+from celery.result import AsyncResult
+from pprint import pprint
+from core import app as celery_app
+
+
+def get_task_progress(task_id: str) -> int:
+    """
+    Return between 0 and 100
+    """
+    result = AsyncResult(task_id)
+    if result.ready():
+        return 100
+    else:
+        task_metadata = celery_app.backend.get_task_meta(result.id)
+        return task_metadata.get('result', {}).get('progress', 1)
+
+
 def sanitize_filename(filename: str) -> str:
     # remove query parameters
     filename = filename.split('?')[0]
